4acbb4faf92d8304a2f286e6eadeb519f8ba644e
Robert Slocumbe <rslocumbe93@gmail.com>
2022-04-28 19:24:01 -0400

    model naming conventions match and work

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -38,7 +38,7 @@ Product.init(
       }
     },
 
-    categoryId: {
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "category",
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,7 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try { 
+    const productData = await Product.findAll({
+      include: Category
+    });
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
